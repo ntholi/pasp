30571e7026c780a901493d3156fb5358209db984
Ntholi Nkhatho <ntholi@live.com>
2023-02-21 15:33:25 +0200

    add 'submissions' to url

--- pasp/urls.py
+++ pasp/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("home.urls")),
     path("admin/", admin.site.urls),
     path("assessments/", include("assessments.urls")),
+    path("submissions/", include("submissions.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
