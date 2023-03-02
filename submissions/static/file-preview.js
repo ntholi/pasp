531acfa8496d204ef83279c832978c6eaf5f8e3b
Ntholi Nkhatho <ntholi@live.com>
2023-03-02 21:45:43 +0200

    add more image and programming language file extensions

--- submissions/static/file-preview.js
+++ submissions/static/file-preview.js
@@ -1,9 +1,31 @@
+const images = ["jpg", "gif", "png", "bmp", "svg", "webp", "tiff", "ico", "heic", "raw"];
+const languages = ["py", "js", "java", "c", "cpp", "cs", "rb", "php", "html", "css", "swift", "go", "r", "sql", "pl", "sh", "kt", "dart", "m", "jl","lua","ts","scala","vb","asp","h","hs","clj","lisp","sas"];
+
+const isImage = (fileExt) => {
+    for (it of images){
+        if (fileExt === it){
+            return true;
+        }
+    }
+    return false;
+}
+
+const isProgrammingLang = (fileExt) => {
+    for (it of languages){
+        if(fileExt === it){
+            return true;
+        }
+    }
+    return false;
+}
+
 const inputElement = document.getElementById("id_attachment")
-const previewBtn = document.getElementById("previewBtn");
 
 const getLanguage = (extension) => {
     let language = 'clike';
-    if (extension === 'css') {
+    if (extension === 'sql') {
+        language = 'language-sql';
+    }if (extension === 'css') {
         language = 'language-css';
     } else if (extension === 'html') {
         language = 'language-html';
@@ -21,33 +43,31 @@ inputElement.addEventListener("change", (event) => {
     const fileExt = file.name.split('.').pop().toLowerCase();
     const previewElement = document.getElementById("preview");
     const previewUrl = URL.createObjectURL(file);
-    if (file.type === "application/pdf") {
+    if (file.name.endsWith(".pdf")) {
         previewElement.innerHTML = `<embed src="${previewUrl}">`;
-    } else if (file.type.startsWith("image/")) {
+    } else if (isImage(fileExt)) {
         previewElement.innerHTML = `<img src="${previewUrl}" alt="Attachment">`;
-    } else if (file.type.startsWith("text")) {
-        if (file.name.endsWith(".txt")) {
-            previewElement.innerHTML = `<iframe src="${previewUrl}"></iframe>`;
-        } else {
-            const language = getLanguage(fileExt)
-            console.log('attachment file type',language)
-            previewElement.innerHTML = `<iframe src="${previewUrl}"></iframe>`;
+    } else if (file.name.endsWith(".txt")) {
+        previewElement.innerHTML = `<iframe src="${previewUrl}"></iframe>`;
+    }else if (isProgrammingLang(fileExt)){
+        const language = getLanguage(fileExt)
+        console.log('attachment file type',language)
+        previewElement.innerHTML = `<iframe src="${previewUrl}"></iframe>`;
 
-            fetch(previewUrl)
-                .then(response => response.text())
-                .then(code => {
-                    const highlightedCode = Prism.highlight(code, Prism.languages.python, language);
-                    previewElement.innerHTML = `<pre><code class="language-python">${highlightedCode}</code></pre>`;
-                });
-        }
+        fetch(previewUrl)
+            .then(response => response.text())
+            .then(code => {
+                const highlightedCode = Prism.highlight(code, Prism.languages.python, language);
+                previewElement.innerHTML = `<pre><code class="language-python">${highlightedCode}</code></pre>`;
+            });
     } else {
         previewElement.innerHTML = `<p>Preview not supported for <code>${fileExt}</code> files</p>`;
     }
 });
 
-// reset the preview if the user clears the file input
-inputElement.addEventListener("click", () => {
-    if (inputElement.value === "") {
-        document.getElementById("preview").innerHTML = "";
-    }
-});
+// // reset the preview if the user clears the file input
+// inputElement.addEventListener("click", () => {
+//     if (inputElement.value === "") {
+//         document.getElementById("preview").innerHTML = "";
+//     }
+// });
