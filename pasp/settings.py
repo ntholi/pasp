4728a15ba8651739ba488832711e0af896bd1fab
Ntholi Nkhatho <ntholi@live.com>
2023-03-02 14:38:27 +0200

    add django-livereload-server to settings.py

--- pasp/settings.py
+++ pasp/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "livereload",
     "crispy_forms",
     "crispy_bootstrap5",
     "home",
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "livereload.middleware.LiveReloadScript",
 ]
 
 ROOT_URLCONF = "pasp.urls"
