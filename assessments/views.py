c8deb218040c13dd10bbc4ebd6b890902c11a45d
Ntholi Nkhatho <ntholi@live.com>
2023-03-04 07:59:01 +0200

    display a table of submissions

--- assessments/views.py
+++ assessments/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import generic
@@ -12,12 +13,16 @@ def index(request):
 
 def details(request, uuid):
     assessment = Assessment.objects.get(uuid=uuid)
+    if not assessment:
+        raise Http404("Assessment does not exist")
     newly_created = request.GET.get("newly_created", 0)
-    print(f"newly_created: {newly_created}")
     return render(
         request,
         "assessments/details.html",
-        {"assessment": assessment, "newly_created": newly_created},
+        {
+            "assessment": assessment,
+            "newly_created": newly_created,
+        },
     )
 
 
